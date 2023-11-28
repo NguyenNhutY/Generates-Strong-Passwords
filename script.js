1b80306d809eca67ec11bf328538e30c3346296a
Nguyễn Bùi Nhựt Ý <130396826+NguyenNhutY@users.noreply.github.com>
2023-11-28 23:34:51 +0700

    script.js

--- script.js
+++ script.js
@@ -9,87 +9,70 @@ const generateEl = document.getElementById("generate");
 
 const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
-const numbers = "0123456789";
-const symbols = "!@#$%^&*()_+=";
+const numericCharacters = "0123456789";
+const symbolCharacters = "!@#$%^&*()_+=";
 
-function getLowercase() {
-    return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
-}
-
-function getUppercase() {
-    return upperLetters[Math.floor(Math.random() * upperLetters.length)];
-}
-
-function getNumber() {
-    return numbers[Math.floor(Math.random() * numbers.length)];
-}
-
-function getSymbol() {
-    return symbols[Math.floor(Math.random() * symbols.length)];
+function getRandomCharacter(characters) {
+    return characters[Math.floor(Math.random() * characters.length)];
 }
 
 function generatePassword() {
     const len = lenEl.value;
+    const selectedTypes = [upperEl, lowerEl, numberEl, symbolEl].filter(el => el.checked);
+
+    if (selectedTypes.length === 0) {
+        alert("Please select at least one character type.");
+        return;
+    }
 
     let password = "";
-
-    if (upperEl.checked) {
-        password += getUppercase();
-    }
-
-    if (lowerEl.checked) {
-        password += getLowercase();
-    }
-
-    if (numberEl.checked) {
-        password += getNumber();
-    }
-
-    if (symbolEl.checked) {
-        password += getSymbol();
-    }
+    selectedTypes.forEach(type => {
+        password += getRandomCharacter(getCharacterSet(type.id));
+    });
 
     for (let i = password.length; i < len; i++) {
-        const x = generateX();
-        password += x;
+        password += getRandomCharacter(getSelectedCharacterTypes());
     }
 
     pwEl.innerText = password;
 }
 
-function generateX() {
-    const xs = [];
-    if (upperEl.checked) {
-        xs.push(getUppercase());
+function getCharacterSet(type) {
+    switch (type) {
+        case "upper":
+            return upperLetters;
+        case "lower":
+            return lowerLetters;
+        case "number":
+            return numericCharacters;
+        case "symbol":
+            return symbolCharacters;
+        default:
+            return "";
     }
+}
 
-    if (lowerEl.checked) {
-        xs.push(getLowercase());
-    }
+function getSelectedCharacterTypes() {
+    const selectedTypes = [];
+    if (upperEl.checked) selectedTypes.push(upperLetters);
+    if (lowerEl.checked) selectedTypes.push(lowerLetters);
+    if (numberEl.checked) selectedTypes.push(numericCharacters);
+    if (symbolEl.checked) selectedTypes.push(symbolCharacters);
 
-    if (numberEl.checked) {
-        xs.push(getNumber());
-    }
-
-    if (symbolEl.checked) {
-        xs.push(getSymbol());
-    }
-
-    if (xs.length === 0) return "";
-
-    return xs[Math.floor(Math.random() * xs.length)];
+    return selectedTypes.join("");
 }
 
 generateEl.addEventListener("click", generatePassword);
 
 copyEl.addEventListener("click", () => {
-    const textarea = document.createElement("textarea");
     const password = pwEl.innerText;
 
     if (!password) {
+        alert("No password to copy.");
         return;
     }
 
+    const textarea = document.createElement("textarea");
     textarea.value = password;
     document.body.appendChild(textarea);
     textarea.select();
